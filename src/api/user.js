59e988c62ef33908af6e694ed4936c541f172411
全志A6 <1942512905@qq.com>
2024-01-06 10:39:26 +0800

    添加登录和修改发布图标

--- src/api/user.js
+++ src/api/user.js
@@ -3,3 +3,8 @@ import request from '@/utils/requests.js';
 export function getUser(conditions) {
     return request.get(`/user/findUserVo/${conditions}`);
 }
+
+//登录
+export function login(data) {
+    return request.post(`/user/login`, data);
+}
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import indexVue from '@/views/index/index.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {path: '/login', component: () => import("../views/login.vue")},
   
     { path: '/', component: indexVue },
     { path: '/index/house', component: () => import("../views/index/house.vue"), },
