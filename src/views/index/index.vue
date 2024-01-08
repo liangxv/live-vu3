1c95b255801711e05ddcc3ec1a89a6678a366f1c
全志A6 <1942512905@qq.com>
2024-01-08 14:11:08 +0800

    首页优化

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
-
+        首页
     </div>
+    <NavBottom></NavBottom>
 </template>
 
 <script setup>
-
+import NavBottom from '@/views/navbottom.vue';
 </script>
 <style scoped>
 
