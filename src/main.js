531e0074cbab1432ed3fcb91c633efae8753dab1
logan <logan@logan.com>
2024-01-09 14:59:58 +0800

    优化发帖页面

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { Uploader,Toast } from 'vant';
+
 
 const app = createApp(App)
 
-app.use(router)
+app.use(router).use(Uploader).use(Toast)
 
 app.mount('#app')
--- src/views/publish/HousePublish.vue
+++ src/views/publish/HousePublish.vue
@@ -1,22 +1,108 @@
 <template>
-    <van-nav-bar title="发布租房买房" left-text="返回" left-arrow @click-left="onClickLeft" />
+    <van-nav-bar title="发布租房买房" left-text="返回" left-arrow @click-left="onClickLeft"/>
+
     <van-form @submit="onSubmit">
-        <van-field name="radio" label="请选择发布类型">
+
+        <van-field v-model="formHouseData.releaseState" name="releaseState" label="请选择发布类型" style="white-space:nowrap;">
             <template #input>
-                <van-radio-group v-model="checked" direction="horizontal">
-                    <van-radio name="1">卖房</van-radio>
-                    <van-radio name="2">出租</van-radio>
-                    <van-radio name="2">求购</van-radio>
-                    <van-radio name="2">求租</van-radio>
+                <van-radio-group v-model="formHouseData.releaseState" direction="horizontal">
+                    <van-radio name=1>卖房</van-radio>
+                    <van-radio name=2>出租</van-radio>
+                    <van-radio name=3>求购</van-radio>
+                    <van-radio name=4>求租</van-radio>
                 </van-radio-group>
             </template>
         </van-field>
-        <van-field label="标题" placeholder="填写标题" />
-        <van-field v-model="result" is-link readonly name="picker" label="房源状态" placeholder="请选择"
-            @click="showPicker = true" />
-        <van-popup v-model:show="showPicker" position="bottom">
-            <van-picker :columns="columns" @confirm="onConfirm" @cancel="showPicker = false" />
+
+        <van-field v-model="formHouseData.title" label="标题" placeholder="填写标题"></van-field>
+
+        <van-field
+                v-model="result"
+                is-link
+                readonly
+                name="houseState"
+                label="房源状态"
+                placeholder="请选择"
+                @click="showPicker1 = true"></van-field>
+
+        <van-popup v-model:show="showPicker1" position="bottom">
+            <van-picker
+                    :columns="columns"
+                    @confirm="onConfirm"
+                    @cancel="showPicker1 = false"/>
+
         </van-popup>
+
+        <van-field v-model="formHouseData.area" label="面积" placeholder="填写面积">
+        </van-field>
+
+        <div class="price-row">
+            <van-field
+                    class="price-field"
+                    v-model="formHouseData.price"
+                    label="房源价格"
+                    placeholder="填写价格"
+            >
+                <template #input>
+                    <van-checkbox-group v-model="formHouseData.isNegotiable" direction="horizontal">
+                        <van-checkbox name="面议" shape="square">面议</van-checkbox>
+                    </van-checkbox-group>
+                </template>
+            </van-field>
+            <div class="manual-price-field" v-if="!formHouseData.isNegotiable.includes('面议')">
+                <van-field
+                        v-model="formHouseData.price"
+                        label="输入价格"
+                        placeholder="填写价格"
+                ></van-field>
+            </div>
+        </div>
+
+        <van-field
+                v-model="result2"
+                name="renovationState"
+                label="装修情况"
+                placeholder="请选择"
+                readonly
+                is-link
+                @click="showPicker2 = true"
+        />
+
+        <van-popup v-model:show="showPicker2" position="bottom">
+            <van-picker
+                    :columns="renovationStateList"
+                    @confirm="onConfirmRenovationState"
+                    @cancel="showPicker2 = false"
+            />
+        </van-popup>
+
+        <van-field
+                v-model="formHouseData.houseAdvantage"
+                name="advantages"
+                label="房源优点"
+                placeholder="例如：离学校近,地铁口附近,随时看房"
+        />
+
+        <van-field
+                v-model="formHouseData.houseDescribe"
+                name="description"
+                label="房源描述"
+                type="textarea"
+                rows="3"
+                autosize
+                placeholder="请输入房源描述"
+        />
+
+        <van-field v-model="formHouseData.imgSrc" name="uploader" label="添加照片">
+            <template #input>
+                <van-uploader :after-read="afterRead" />
+            </template>
+        </van-field>
+
+        <van-field v-model="formHouseData.person" label="联系人" placeholder="请输入联系人姓名"/>
+
+        <van-field v-model="formHouseData.contact" label="电话" placeholder="请输入联系人电话" type="tel"/>
+
 
         <div style="margin: 16px;">
             <van-button round block type="primary" native-type="submit">
@@ -25,24 +111,110 @@
         </div>
     </van-form>
 </template>
+
+<style>
+    .price-row {
+        display: flex;
+        align-items: center;
+    }
+
+    .price-field {
+        flex: 1;
+        margin-right: 10px;
+    }
+
+    .manual-price-field {
+        flex: 1;
+    }
+</style>
+
 <script setup>
-import { ref } from 'vue';
-const onClickLeft = () => history.back();
+    import {ref} from 'vue';
+    import {houseAdd} from "../../api/house";
 
-const checked = ref('1');
 
-const result = ref('');
-const showPicker = ref(false);
-const columns = [
-    { text: '新房', value: '1' },
-    { text: '二手房', value: '2' },
-    { text: '租房', value: '3' },
-];
+    const onClickLeft = () => history.back();
 
-const onConfirm = ({ selectedOptions }) => {
-    result.value = selectedOptions[0]?.text;
-    showPicker.value = false;
-};
+    const areaValue = ref('120㎡一室一厅一卫');
+
+    const checked = ref('1');
+    const result = ref('');
+    const result2 = ref('');
+
+    const imagevalue=ref([
+        {url:'https://fastly.jsdelivr.net/npm/@vant/assets/leaf.jpeg'}
+    ]);
+
+    const afterRead=(file)=>{
+        // console.log("文件信息:"+file)
+        console.log(file)
+        console.log("上传成功!")
+        console.log(file.objectUrl)
+
+        // console.log("文件名："+file.file.name)
+        // console.log("文件大小：" + file.file.size);
+        // console.log("文件类型：" + file.file.type);
+        // console.log("文件路径：" + file.file.url);
+        // formHouseData.value.imgSrc=URL.createObjectURL(file.file)
+    };
+
+    const formHouseData = ref({
+        isNegotiable: [],
+        price: '',
+        releaseState: '',
+        title: '',
+        houseState: '',
+        area: '',
+        renovationState: '',
+        houseAdvantage: '',
+        houseDescribe: '',
+        imgSrc: '',
+        person: '',
+        contact: '',
+    });
+
+    let showPicker1 = ref(false);
+    let showPicker2 = ref(false);
+
+    const columns = [
+        {text: '新房', value: '1'},
+        {text: '二手房', value: '2'},
+        {text: '租房', value: '3'},
+    ];
+
+    const renovationStateList = [
+        {text: '简装修', value: '1'},
+        {text: '精装修', value: '2'},
+        {text: '毛坯房', value: '3'},
+    ];
+
+
+    const onConfirm = ({selectedOptions}) => {
+        result.value = selectedOptions[0]?.text;
+        formHouseData.value.houseState = selectedOptions[0].value;
+        showPicker1.value = false;
+    };
+
+    const onConfirmRenovationState = ({selectedOptions}) => {
+        result2.value=selectedOptions[0]?.text
+        formHouseData.value.releaseState = selectedOptions[0]?.value
+        showPicker2.value = false;
+    };
+
+    const onSubmit = async () => {
+        const HouseData={
+
+        };
+      let formData=new FormData;
+
+      formData.append("houseRental",JSON.stringify(formHouseData.value));
+      console.log(formData.get("avatarFile"))
+      const response=await houseAdd(formData);
+
+      if (response.data.code===200){
+          
+      }
+
+    };
 
 </script>
-<style scoped></style>
